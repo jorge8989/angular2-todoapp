66acf55c57e4ffff351cf5afad5dc01e1517864e
jorge <jorgearteaga8936@gmail.com>
2017-06-04 22:41:19 -0400

    added express server

---
+++ serverApp.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', process.env.PORT || 3010);
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('app running on port '+app.get('port'));
+});
